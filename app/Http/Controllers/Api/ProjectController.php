5b28368ed8e3c7207185462f43f20a2278f69e06
FrancescoPepi <francescopepi16@gmail.com>
2023-11-08 19:13:47 +0100

    Add Api

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $projects = Project::select('id', 'name', 'slug')->paginate(10);
+        return response()->json($projects);
+    }
+
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+}
