6a9a2cfb94c6a25b24e4e5e7b0366808b75ba82b
Francesco Pepi <133686074+FrancescoPepi@users.noreply.github.com>
2023-11-08 18:25:52 +0100

    Initial commit

---
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreProjectRequest;
+use App\Http\Requests\UpdateProjectRequest;
+use App\Models\Project;
+use App\Models\Category;
+use App\Models\Tag;
+use Illuminate\Support\Str;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $category = Category::all();
+        $projects = Project::orderBy("id", "desc")->paginate(10);
+        return view("admin.projects.index", compact("projects", "category"));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $project = new Project;
+        $tags = Tag::orderBy('label')->get();
+        $categories = Category::all();
+        return view("admin.projects.create", compact("categories", "project", "tags"));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreProjectRequest $request)
+    {
+        $data = $request->validated();
+        $project = new Project;
+        $project->fill($data);
+        $project->slug = Str::slug($project->name);
+        $project->save();
+
+        if (Arr::exists($data, "tags"))
+            $project->tags()->attach($data["tags"]);
+        return redirect()->route("admin.projects.index");
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Project  $project
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Project $project)
+    {
+        return view("admin.projects.show", compact("project"));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Project  $project
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Project $project)
+    {
+        $tags = Tag::orderBy('label')->get();
+        $project_tags = $project->tags->pluck('id')->toArray();
+        $categories = Category::all();
+        return view("admin.projects.edit", compact("project", "categories", "tags", "project_tags"));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Project  $project
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateProjectRequest $request, Project $project)
+    {
+        $data = $request->validated();
+        $project->update($data);
+        $project->slug = Str::slug($project->name);
+        if (Arr::exists($data, "tags"))
+            $project->tags()->sync($data["tags"]);
+        else
+            $project->tags()->detach();
+        $project->save();
+
+        return redirect()->route("admin.projects.show", $project);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Project  $project
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Project $project)
+    {
+        $project->tags()->detach();
+        $project->delete();
+        return redirect()->route("admin.projects.index");
+    }
+}
